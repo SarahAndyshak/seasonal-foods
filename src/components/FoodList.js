85311ff62a5a32ef7238940427d958e7b7a1f4f3
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-05-05 14:13:06 -0700

    Tried using code directly from tutorial, still skewed

--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -23,12 +23,18 @@ const StyledFoodList = styled.div`
     height: 100px;
   }
 
+  .place_list {
+    position: absolute;
+    z-index: 1;
+    padding: 20px;
+    box-shadow: 0px 8px 10px 0px #0000002b;
+  }
+
   .circle-container {
     position: relative;
     width: 600px;
     height: 600px;
     padding: 20px;
-    // /*2.8em = 2em*1.4 (2em = half the width of a link with img, 1.4 = sqrt(2))*/
     border: solid 1px;
     border-radius: 50%;
     margin: 60px auto;
@@ -42,23 +48,33 @@ const StyledFoodList = styled.div`
     height: 50px;
     margin: -10px;
   }
+  // .circle-container img { display: block; width: 100%; }
+  // .deg0 { transform: translate(200px); }
+  // .deg24 { transform: rotate(24deg) translate(200px) rotate(-24deg); }
+  // .deg48 { transform: rotate(48deg) translate(200px) rotate(-48deg); }
+  // .deg72 { transform: rotate(72deg) translate(200px) rotate(-72deg); }
+  // .deg96 { transform: rotate(96deg) translate(200px) rotate(-96deg); }
+  // .deg120 { transform: rotate(120deg) translate(200px) rotate(-120deg); }
+  // .deg144 { transform: rotate(144deg) translate(200px) rotate(-144deg); }
+  // .deg168 { transform: rotate(168deg) translate(200px) rotate(-168deg); }
+  // .deg192 { transform: rotate(192deg) translate(200px) rotate(-192deg); }
+  // .deg216 { transform: rotate(216deg) translate(200px) rotate(-216deg); }
+  // .deg240 { transform: rotate(240deg) translate(200px) rotate(-240deg); }
+  // .deg264 { transform: rotate(264deg) translate(200px) rotate(-264deg); }
+  // .deg288 { transform: rotate(288deg) translate(200px) rotate(-288deg); }
+  // .deg312 { transform: rotate(312deg) translate(200px) rotate(-312deg); }
+  // .deg336 { transform: rotate(336deg) translate(200px) rotate(-336deg); }
+  
+
   .circle-container img { display: block; width: 100%; }
   .deg0 { transform: translate(200px); }
-  .deg24 { transform: rotate(24deg) translate(200px) rotate(-24deg); }
-  .deg48 { transform: rotate(48deg) translate(200px) rotate(-48deg); }
-  .deg72 { transform: rotate(72deg) translate(200px) rotate(-72deg); }
-  .deg96 { transform: rotate(96deg) translate(200px) rotate(-96deg); }
-  .deg120 { transform: rotate(120deg) translate(200px) rotate(-120deg); }
-  .deg144 { transform: rotate(144deg) translate(200px) rotate(-144deg); }
-  .deg168 { transform: rotate(168deg) translate(200px) rotate(-168deg); }
-  .deg192 { transform: rotate(192deg) translate(200px) rotate(-192deg); }
-  .deg216 { transform: rotate(216deg) translate(200px) rotate(-216deg); }
-  .deg240 { transform: rotate(240deg) translate(200px) rotate(-240deg); }
-  .deg264 { transform: rotate(264deg) translate(200px) rotate(-264deg); }
-  .deg288 { transform: rotate(288deg) translate(200px) rotate(-288deg); }
-  .deg312 { transform: rotate(312deg) translate(200px) rotate(-312deg); }
-  .deg336 { transform: rotate(336deg) translate(200px) rotate(-336deg); }
-  
+  .deg45 { transform: rotate(45deg) translate(200px) rotate(-45deg); }
+  .deg90 { transform: rotate(90deg) translate(200px) rotate(-90deg); }
+  .deg135 { transform: rotate(135deg) translate(200px) rotate(-135deg); }
+  .deg180 { transform: translate(-200px); }
+  .deg225 { transform: rotate(225deg) translate(200px) rotate(-225deg); }
+  .deg270 { transform: rotate(270deg) translate(200px) rotate(90deg); }
+  .deg315 { transform: rotate(315deg) translate(200px) rotate(-315deg); }
 `
 
 function FoodList() {
@@ -66,6 +82,8 @@ function FoodList() {
     <StyledFoodList>
       <a href="https://www.usdalocalfoodportal.com/fe/fdirectory_farmersmarket/?source=fe&directory=farmersmarket&location=&x=&y=">Find a Farmers Market Near You!</a>
       <br /> <br />
+
+{/* Just placing on the page */}
       {/* <img alt="cherry" src={Cherry}/>
       <img alt="tomato" src={Tomato}/>
       <img alt="carrots" src={Carrots}/>
@@ -82,6 +100,7 @@ function FoodList() {
       <img alt="dragonfruit" src={Dragonfruit}/>
       <img alt="watermelon" src={Watermelon}/> */}
 
+{/* Placing images in a circle */}
     <div className='circle-container'>
       
       {/* <Link to="/food">
@@ -89,8 +108,7 @@ function FoodList() {
       </Link> */}
 
       {/* <img src='image.jpg'className='center'/> */}
-
-      <img alt="cherry" src={Cherry} className='deg0'/>
+      {/* <img alt="cherry" src={Cherry} className='deg0'/>
       <img alt="tomato" src={Tomato} className='deg24'/>
       <img alt="carrots" src={Carrots} className='deg48'/>
       <img alt="orange" src={Orange} className='deg72'/>
@@ -104,33 +122,18 @@ function FoodList() {
       <img alt="cabbage" src={Cabbage} className='deg264'/>
       <img alt="grapes" src={Grapes} className='deg288'/>
       <img alt="dragonfruit" src={Dragonfruit} className='deg312'/>
-      <img alt="watermelon" src={Watermelon} className='deg336'/>
+      <img alt="watermelon" src={Watermelon} className='deg336'/> */}
+
+      <img alt="cherry" src={Cherry} className='deg0'/>
+      <img alt="tomato" src={Tomato} className='deg45'/>
+      <img alt="carrots" src={Carrots} className='deg90'/>
+      <img alt="orange" src={Orange} className='deg135'/>
+      <img alt="mango" src={Mango} className='deg180'/>
+      <img alt="banana" src={Banana} className='deg225'/>
+      <img alt="kiwi" src={Kiwi} className='deg270'/>
+      <img alt="avocado" src={Avocado} className='deg315'/>
+
     </div>
-
-    {/* <div className='circle-container'>
-      <a href='#' class='center'><img src='image.jpg'/></a>
-      
-      <Link to="/food">
-        <img alt="cherry" src={Cherry} className='deg0'/>
-      </Link>
-
-
-      <a href='#' class='deg24'><img alt="tomato" src={Tomato}/></a>
-      <a href='#' class='deg48'><img alt="carrots" src={Carrots}/></a>
-      <a href='#' class='deg72'><img alt="orange" src={Orange}/></a>
-      <a href='#' class='deg96'><img alt="mango" src={Mango}/></a>
-      <a href='#' class='deg120'><img alt="banana" src={Banana}/></a>
-      <a href='#' class='deg144'><img alt="kiwi" src={Kiwi}/></a>
-      <a href='#' class='deg168'><img alt="avocado" src={Avocado}/></a>
-      <a href='#' class='deg192'><img alt="broccoli" src={Broccoli}/></a>
-      <a href='#' class='deg216'><img alt="cauliflower" src={Cauliflower}/></a>
-      <a href='#' class='deg240'><img alt="blueberries" src={Blueberries}/></a>
-      <a href='#' class='deg264'><img alt="cabbage" src={Cabbage}/></a>
-      <a href='#' class='deg288'><img alt="grapes" src={Grapes}/></a>
-      <a href='#' class='deg312'><img alt="dragonfruit" src={Dragonfruit}/></a>
-      <a href='#' class='deg336'><img alt="watermelon" src={Watermelon}/></a>
-    </div> */}
-
 
 
     </StyledFoodList>
