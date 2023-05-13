827e2b4c56df58a40365e8e34eb6266dd768f856
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-05-12 17:00:53 -0700

    Add font and preliminary style to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,24 @@ import styled from "styled-components";
 
 const StyledHeader = styled.header`
   background-color: rgb(185, 223, 152);
+  font-family: 'Fredericka the Great', cursive;
+
+  & > h1 {
+    font-size: 3rem;
+  }
+
+  
+  & > p {
+    font-size: 2rem;
+  }
+
+  & a {
+    color:rgb(73, 97, 51)
+  }
+  
+  & a:hover {
+    color:rgb(124, 55, 147)
+  }
 `
 
 function Header() {
@@ -11,7 +29,7 @@ function Header() {
     <>
       <StyledHeader>
       <h1>Is it in season?</h1>
-      <Link to="/">Home</Link>
+      <p><Link to="/">Home</Link></p>
       </StyledHeader>
     </>
   )
